2e6e088e25cd1632c471e07c23be5be7210fcf12
chernandez260391-cyber <chernandez260391@gmail.com>
2026-07-14 23:38:39 -0400

    Update info_personal.py

--- info_personal.py
+++ info_personal.py
@@ -1,5 +1,5 @@
-nombre = "Ingresa tu nombre aquí"
-rut = "Ingresa tu RUT aquí"
+nombre = "CARLOS ALFREDO HERNANDEZ AGUILERA"
+rut = "17.862.149-1"
 
 print(f"Nombre Completo: {nombre}")
 print(f"RUT: {rut}")
